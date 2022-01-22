e69a97aed1bb5e03c8401a9ab9cd3ee7cfbf2135
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-22 14:11:21 +0100

    adding validations

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -12,10 +12,16 @@ const AddUser = (props) => {
   const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (!isFormValid()) {
+        alert('Form is not valid')
+        return
+    }
     console.log('Form', {
         name: enteredName,
         age: enteredAge
     })
+    setEnteredName('')
+    setEnteredAge('')
   };
 
   const nameChangedHandler = (event) => {
@@ -26,13 +32,17 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value)
   }
 
+  const isFormValid = () => {
+    return (enteredName.trim().length > 0 && enteredAge.trim().length > 0) && +enteredAge > 0
+  }
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">What is the name?</label>
-        <input type="text" id="username" onChange={nameChangedHandler} />
+        <input type="text" id="username" value={enteredName} onChange={nameChangedHandler} />
         <label htmlFor="age">How old is she/he?</label>
-        <input type="number" id="age" onChange={ageChangedHandler} />
+        <input type="number" id="age" value={enteredAge} onChange={ageChangedHandler} />
         <Button type="submit">Send</Button>
       </form>
     </Card>
